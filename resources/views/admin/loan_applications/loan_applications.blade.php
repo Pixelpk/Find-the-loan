7efc436243a0d477c5b6b408887da74c86daa9d4
Atif Raza <paarasalitaimoor@gmail.com>
2021-12-24 19:06:21 +0500

    merge

--- resources/views/admin/loan_applications/loan_applications.blade.php
+++ resources/views/admin/loan_applications/loan_applications.blade.php
@@ -192,9 +192,7 @@
                                             @foreach($applications as $application)
                                             <tr class="loan_application_row  @if($application->assigned_application != null && $application->assigned_application->status == 1 && Auth::user()->parent_id != 0) shake @endif" url="{{ route('loan-application-summary',['apply_loan_id'=>$application->id]) }}" title="Show summary"  style="cursor: pointer;">
                                                 <td class="selected_application">
-                                                    @if($application->assigned_to_user == null && $application->application_quote == null && $application->application_rejected == null)
                                                     <input style="height: 16px;width: 16px" name="selected_application" class="form-control" value="{{$application->id}}" id="application{{$application->id}}" type="checkbox"/>
-                                                    @endif
                                                 </td>
                                                 <td>{{ $application->enquiry_id }}</td>
                                                 {{-- <td>
