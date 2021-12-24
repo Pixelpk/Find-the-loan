0fdc4ed4f19cdfed045309cf3a85b718883d78d0
umarr <74894721+Omer-Omer@users.noreply.github.com>
2021-12-24 14:58:33 +0500

    hire_purchase_changes

--- resources/views/admin/pages/modals.blade.php
+++ resources/views/admin/pages/modals.blade.php
@@ -147,7 +147,7 @@
                         <select required style="width: 100%;" multiple class="form-control select2" name="property_types[]" id="property_types">
                                 <option value="3">Commercial Vehicle – Cars, lorries, trucks etc</option>
                                 <option value="4">Industry Vehicle – Cranes, forklift, Tractors etc</option>
-                                <option value="5">Other Commercial & Industrial Equipment</option>
+                                <option value="5">Passenger Vehicle</option>
                         </select>
                     </div>
                     <div class="form-group">
